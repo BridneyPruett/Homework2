5d0fe7d106f23fb9a649761c40f52a5c598e401a
BridneyPruett <59849987+BridneyPruett@users.noreply.github.com>
2020-02-08 16:38:59 -0500

    started creating a person class

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    private String ID;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private int YOB;
+
+    public String fullName(){
+        return firstName;
+    }
+
+}
