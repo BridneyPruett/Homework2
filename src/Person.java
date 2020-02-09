b4cffdee0872d4ebfaae3b8605996f885c654cb6
BridneyPruett <59849987+BridneyPruett@users.noreply.github.com>
2020-02-09 13:57:30 -0500

    Created a person class

--- src/Person.java
+++ src/Person.java
@@ -3,10 +3,68 @@ public class Person {
     private String firstName;
     private String lastName;
     private String title;
+    private String fullName;
     private int YOB;
 
-    public String fullName(){
+    public Person(){
+        this.ID = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.title = "";
+        this.fullName = "";
+        this.YOB = 1991;
+    }
+
+    public Person(String ID, String firstName, String lastName, int YOB, String title){
+        this.ID = ID;
+        this.firstName = firstName;
+        this.YOB = YOB;
+        this.lastName = lastName;
+        this.title = title;
+
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+    public void names(String fullName){
+        this.fullName = firstName + lastName;
+    }
 }
